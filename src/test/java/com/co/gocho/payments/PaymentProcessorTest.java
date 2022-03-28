f38df02ab72c398040590ec6ef9df1de7bcc3fb8
admin <ti@ospedale.com.co>
2022-03-28 15:16:55 -0500

    (10) Análisis de los tests y mejoras
    Nuestros test siguen un mismo proceso:
    
    Se preparan los objetos que vamos a probar.
    Llamamos al método que estamos probando.
    Comprobamos los resultados.
    Podemos reducir la cantidad de código moviendo las partes comunes de preparación a una función que se ejecute antes de cada prueba.
    Con @Before le indicamos a JUnit la función que debe ejecutar antes de cada prueba.
    
    Aporte: Las 3 A de los Test
    
    Arrange (Preparar)
    Act (Actuar)
    Assert (Afirmar)

--- src/test/java/com/co/gocho/payments/PaymentProcessorTest.java
+++ src/test/java/com/co/gocho/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package com.co.gocho.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -7,13 +8,19 @@ import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
 
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setup(){
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     public void payment_is_correct(){
-
-
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+
         boolean result = paymentProcessor.makePayment(1000);
         assertTrue(result);
     }
@@ -22,9 +29,8 @@ public class PaymentProcessorTest {
     @Test
     public void payment_is_wrong(){
 
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+
         boolean result = paymentProcessor.makePayment(1000);
         assertFalse(result);
     }
