ed4f88441c31921114826f5691c9a68e8f256947
admin <ti@ospedale.com.co>
2022-04-03 15:58:00 -0500

    (20) Test de integración con base de datos: guardar películas y búsqueda de películas individuales"
    Por el momento, nuestras pruebas están cargando varias veces la información de la base de datos y vamos a solucionar este problema creando una función que borre la información. Para esto, debemos usar la instrucción @After para que JUnit ejecute la función cada vez que termina de hacer un test.

--- src/main/java/com/co/gocho/movies/data/MovieRepositoryJdbcImpl.java
+++ src/main/java/com/co/gocho/movies/data/MovieRepositoryJdbcImpl.java
@@ -19,8 +19,7 @@ public class MovieRepositoryJdbcImpl implements MovieRepository {
 
     @Override
     public Movie findById(long id) {
-        Object[] args = {id};
-        return jdbcTemplate.queryForObject("select * from movies where id = ?",args,movieMapper);
+        return null;
     }
 
     @Override
@@ -31,9 +30,6 @@ public class MovieRepositoryJdbcImpl implements MovieRepository {
 
     @Override
     public void saveOrUpdate(Movie movie) {
-
-        jdbcTemplate.update("insert into movies(name,minutes,genre) values(?,?,?)",
-                movie.getName(),movie.getMinutes(),movie.getGenre().toString());
 
     }
 
